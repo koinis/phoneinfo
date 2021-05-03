b926dfcf4ce6fd4e5e696e7986aee179f5ef4348
mrpol <pkoinis8@gmail.com>
2021-05-03 21:29:53 +0300

    Update phoneinfo.py

--- phoneinfo.py
+++ phoneinfo.py
@@ -1,87 +1,49 @@
-#mrpol#
-
 import phonenumbers
-
+from art import *
 from phonenumbers import geocoder
 from phonenumbers import carrier
 from phonenumbers import timezone
 from datetime import datetime
+ 
+phoneinfo = text2art("phoneinfo")
+print(phoneinfo)
 
-#Ask the user if he wants info about a phone number
-question = input("Do you want info about a phone number? (y/n): ")
-check = True
+number = input("Enter a phone number: ")
 
-while check == True:
 
-    if question=="y":
-            
-        check = False
-        device = input("Do you want info about a Mobile phone or Home phone number? (m/h): ")   #Ask the type of the phone number in order to print the right note
-            
-        if device == "h":
-            print("Note: Carrier cannot be found")
-                
-        elif device == "m":
-            print("Note: Carrier can be found")
-                
-        else:
-            break
-                    
-        number = input("Enter a phone number: ") #Ask the phone number (The number which is given must starts with the right country code (eg +30 for Greece, +34 for Spain, etc.)
-                
-        try:
-            phone_number = phonenumbers.parse(number, "EU")
+try:
+            phone_number = phonenumbers.parse(number, "GR")
 
             print("Number: ", number)
-            print("Country or City: ", geocoder.description_for_number(phone_number, "en")) #print the Country (or the City if it is a home phone number) in which the number is registered
-                                
-            service_number = phonenumbers.parse(number, "EU")
+            print("Country or City: ", geocoder.description_for_number(phone_number, "en")) #prints the Country (or the City if it is a home phone number) in which the number is registered
+                                                
+            service_number = phonenumbers.parse(number, "GR")
 
-            print("Carrier: ", carrier.name_for_number(service_number, "en")) #print the carrier of the phone number (It works with mobile numbers)
-                                
-            phone_number = phonenumbers.parse(number, "EU")
-                    
+            print("Carrier: ", carrier.name_for_number(service_number, "en")) #prints the carrier of the phone number (It works with mobile numbers)
+                                                
+            phone_number = phonenumbers.parse(number, "GR")
+                                    
             print("Timezone :", timezone.time_zones_for_number(phone_number)) #print the timezone that the number belongs
 
             phone_number = phonenumbers.parse(number)
-                    
-            valid = phonenumbers.is_valid_number(phone_number) #check if it is a valid phone number
-            possible = phonenumbers.is_valid_number(phone_number) #check if it is a possible phone number
+                                    
+            valid = phonenumbers.is_valid_number(phone_number) #checks if it is a valid phone number
+            possible = phonenumbers.is_valid_number(phone_number) #checks if it is a possible phone number
 
             if valid==True and possible==True:
-                print("This is a valid and possible phone number") #print that it is a valid and a possible phone number
-                    
+                print("This is a valid and possible phone number")
+                                    
             else:
-                print("This is not a valid phone number (Note: Potentially dangerous phone number)") #print that it is not a valid phone number (potentially dangerous)
-
+                print("This is not a valid phone number (Note: Potentially dangerous phone number)")
             now = datetime.now()
             date_str = now.strftime("%d/%m/%Y %H:%M:%S")
-            print("Date and time of searching:", date_str) #print date and time of searching
-                    
-        except Exception:
-            print("Error!")
-            print("Country or city not found")
-            print("Carrier not found")
-            check = True
-            q = input("Do you want to continue? (y/n): ")
-                
-            if q == "n":
-                check = False
-                print("Thanks, bye!")
-            elif q == "y":
-                check = True
-            else:
-                break
-
-    elif question=="n":
-        print("Sorry,I can't help you.")
-        break
-    else:
-        break
-
+            print("Date and time of searching:", date_str) #prints the date and the time of searching
+                                    
+except Exception:
+    print("Error!")
+    print("Country or city not found")
+    print("Carrier not found")
 
     
-    
-        
-            
-    
+message = text2art("Made by mrpol")
+print(message)
